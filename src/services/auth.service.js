406eaac7eab62d107ec8bf544df4e75843858c8f
LucianoRaulToledo1978 <lucianotoledoraul@gmail.com>
2025-10-13 22:12:30 -0300

    bakend

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -78,7 +78,13 @@ class AuthService{
         const user = await UserRepository.getByEmail(email)
         if(!user){
             throw new ServerError(404, 'Email no registrado')
+
         }
+
+        if (user.verified_email===false){
+            throw new ServerError(401,'Email no verificado')
+        }
+
         /* Permite saber si cierto valor es igual a otro cierto valor encriptado */
         const is_same_password = await bcrypt.compare(password, user.password)
         if(!is_same_password){
