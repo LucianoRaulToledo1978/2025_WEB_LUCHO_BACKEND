a711c4d8c9880348e8ba2b4df7f65ce8fa1c1c84
LucianoRaulToledo1978 <lucianotoledoraul@gmail.com>
2025-11-25 12:28:13 -0300

    Fix env issues

--- src/config/environment.config.js
+++ src/config/environment.config.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 const ENVIRONMENT = {
     MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
@@ -8,7 +8,6 @@ const ENVIRONMENT = {
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
     URL_API_BACKEND: process.env.URL_API_BACKEND,
     URL_FRONTEND: process.env.URL_FRONTEND
-}
+};
 
-
-export default ENVIRONMENT
+export default ENVIRONMENT;
